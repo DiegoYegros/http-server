53b911971549fd919c07d9e94d7e25e4033c8230
Diego Yegros <diegoyegrosa@gmail.com>
2024-06-24 23:55:35 -0400

    playing a little bit more :3

--- config/config.go
+++ config/config.go
@@ -2,15 +2,28 @@ package config
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v3"
 	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
 	Server struct {
-		Host string `yaml:"host"`
-		Port int    `yaml:"port"`
+		Host         string        `yaml:"host"`
+		Port         int           `yaml:"port"`
+		ReadTimeout  time.Duration `yaml:"read_timeout"`
+		WriteTimeout time.Duration `yaml:"write_timeout"`
+		IdleTimeout  time.Duration `yaml:"idle_timeout"`
 	} `yaml:"server"`
+	Logging struct {
+		Level string `yaml:"level"`
+		File  string `yaml:"file"`
+	} `yaml:"logging"`
+	Database struct {
+		Driver           string `yaml:"driver"`
+		ConnectionString string `yaml:"connection-string"`
+	} `yaml:"database"`
 }
 
 func LoadConfig(filename string) (*Config, error) {
@@ -23,5 +36,25 @@ func LoadConfig(filename string) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("in file %q: %v", filename, err)
 	}
+
+	c.setDefaults()
 	return c, nil
+}
+
+func (c *Config) setDefaults() {
+	if c.Server.Host == "" {
+		c.Server.Host = "127.0.0.1"
+	}
+	if c.Server.Port == 0 {
+		c.Server.Port = 8080
+	}
+	if c.Server.ReadTimeout == 0 {
+		c.Server.ReadTimeout = 15 * time.Second
+	}
+	if c.Server.WriteTimeout == 0 {
+		c.Server.WriteTimeout = 15 * time.Second
+	}
+	if c.Server.IdleTimeout == 0 {
+		c.Server.IdleTimeout = 15 * time.Second
+	}
 }
